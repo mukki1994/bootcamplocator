a047b8bb12073c3c4791e1b1a5375414c30eeadf
mukki1994 <mukesh.rawat@worksdelight.com>
2018-08-02 17:42:57 +0530

    done

---
+++ app/src/main/java/com/bootcamplocator/app/MapsActivity.java
@@ -0,0 +1,107 @@
+package com.bootcamplocator.app;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import com.google.android.gms.location.LocationListener;
+import android.se.omapi.SEService;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks,LocationListener {
+
+
+    private GoogleApiClient mGoogleApiClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.container);
+        if (mapFragment == null){
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.container,mapFragment).commitAllowingStateLoss();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("location_changed","logitude"+location.getLongitude()+" latitude"+location.getLatitude());
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},101);
+                Log.e("request","user requesting permission"+" ");
+            }else {
+                startLocationService();
+                Log.e("started","started location service from onConnected"+" ");
+            }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void startLocationService(){
+        Log.e("Called","starting location service called");
+
+        try{
+            LocationRequest locationRequest = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, (LocationListener) this);
+        }catch (SecurityException exception){
+            Log.e("LOC",exception.toString());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 101:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startLocationService();
+                }else {
+
+                }
+            }
+        }
+    }
+}
